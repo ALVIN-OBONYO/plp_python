ed9c62a48b1181c614f92747e0b2bd0f572b42e2
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-02-26 22:28:02 +0300

    plp ass

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# name = input ("Alvin ")
+# # print (name)
+# age = input ("twenty ")
+# location = input ("Nairobi Kenya.")
+
+# "Hello [name], you are [age] years old and live in [location]"
+
+
+name = "Alvin O"
+age = 20
+location = "Nairobi, Kenya"
+
+print (f"Hello {name}, you are {age} years old and live in {location}")
+
