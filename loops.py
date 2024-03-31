a353440d3c6ff7880dbeb894846248366bfc0b21
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-03-31 14:30:37 +0300

    fix (loop) ammend

--- loops.py
+++ loops.py
@@ -5,9 +5,51 @@ welcome_message = "Welcome to the loops.py"
 for i in range(5):
     print("Welcome message")
 
-
+# infinite number
     count = 0
     while count <= 7:
         # Loop Body
         print (count)
         count += 1
+
+# clothes loop
+    
+    colors = ["red", "green", "blue", "white", "brown", "cream"]
+    color_i_want = "blue"
+
+                            #for loop
+
+    # for color in colors:
+
+    #     if color == color_i_want:
+    #         print("I have found the color i want")
+    #         break
+    #     print(color)
+
+
+              # while loop        
+length = len(colors)
+count = 0
+
+while count < length:
+    print (colors[count])
+
+    if colors[count] == color_i_want:
+        print("I have found the color i want")
+        break
+    count += 1
+
+
+        #Age entrance in a club
+ages = [12, 15, 18, 20, 25, 30, 32, 38]
+
+for age in ages:
+    if age <21:
+        continue
+    print (age)
+
+    #Nested loops
+    groups = [["Paul", "skinny"], ["Annette", "George"]]
+    for group in groups:
+        for name in group:
+            print(name)
