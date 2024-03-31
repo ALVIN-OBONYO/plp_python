873f56f1813ad7a28851a14084828b1ff04c3b29
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-03-31 14:11:36 +0300

    infinite loop

---
+++ loops.py
@@ -0,0 +1,13 @@
+#for
+
+welcome_message = "Welcome to the loops.py"
+
+for i in range(5):
+    print("Welcome message")
+
+
+    count = 0
+    while count <= 7:
+        # Loop Body
+        print (count)
+        count += 1
