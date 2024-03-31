cf3eaed3d43d4f5d03609975fb3a9dd47da08b02
ALVIN-OBONYO <alvinnmichael@gmail.com>
2024-03-31 13:49:12 +0300

    conditional statment

---
+++ plp_week3.py
@@ -0,0 +1,36 @@
+Mary = 20
+
+if Mary > 18:
+    print("Mary can vote")
+
+
+grade = 75
+
+if grade >= 90:
+    print("A")
+elif grade >= 80:
+    print("B")
+elif grade >= 70:
+    print("C")
+elif grade >= 60:
+    print("D")
+else:
+    print("F")
+
+    
+
+
+
+
+
+weekday = "Monday" , "Tuesdy" , "Wednesday" , "Thursday" , "Friday"
+Weekend = "Saturday" , "Sunday"
+
+Weekday = "Sunday"
+
+
+if Weekday:
+    print("Wake up at 6:30")
+else: 
+    print("sleep in")
+
